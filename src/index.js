a8b142e34ef1ac75805d3c068a83c524b70f6004
nahuelraverta <107009596+nahuelraverta@users.noreply.github.com>
2022-12-05 22:41:29 -0300

    sumar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAaSXnm9Zx_Xix7HCUdaQOVo2oFzo1-_jk",
+  authDomain: "comixnahue.firebaseapp.com",
+  projectId: "comixnahue",
+  storageBucket: "comixnahue.appspot.com",
+  messagingSenderId: "566126202703",
+  appId: "1:566126202703:web:f9635979d4657b648c3f3a",
+  measurementId: "G-62EG7G306Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +37,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
